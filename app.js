b66d9e085895bb8131200a08946917653859506b
Max Sheyn <nmmn1905@gmail.com>
2026-03-10 18:50:46 +0200

    feat: add practice tasks for OR (||) and NOT (!) operators

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ==========================================
-// ТЕМА: Логічний оператор "І" (Logical AND: &&)
+// Логічний оператор "І" (Logical AND: &&)
 // ==========================================
 let cartTotal = 1000;
 let userRegister = true;
@@ -27,4 +27,63 @@ if (userAge >= 18 && userPasport === true) {
   console.log(`Ласкаво просимо на борт`);
 } else {
   console.log(`Заборона на посадку, зверніться до персоналу`);
+}
+
+// ==========================================
+// Логічний оператор "АБО" (Logical OR: ||)
+// ==========================================
+let isStudent = false;
+let clientAge = 16;
+if (clientAge < 18 || isStudent === true) {
+  console.log(`Ваша знижка 10% Приємного перегляду`);
+} else {
+  console.log(`Приємного перегляду`);
+}
+
+// ===
+
+let isBirthday = false;
+let hasPromoCode = false;
+if (hasPromoCode === true || isBirthday === true) {
+  console.log(`Вам надано промо-код `);
+} else {
+  console.log(`Наразі у вас відсутній промо-код`);
+}
+
+// ===
+
+let motionDetected = false;
+let isDarkOutside = true;
+if (isDarkOutside === true || motionDetected === true) {
+  console.log(`Light ON`);
+} else {
+  console.log(`Light OFF`);
+}
+
+// ==========================================
+// Логічний оператор "НЕ" (Logical NOT: !)
+// ==========================================
+let hasVirus = false;
+if (!hasVirus) {
+  console.log(`Вірус не знайдено`);
+} else {
+  console.log(`Знайдено вірус`);
+}
+
+// ===
+
+let hasPremium = false;
+if (!hasPremium) {
+  console.log(`Show first ads video`);
+} else {
+  console.log(`Show video without ads`);
+}
+
+// ===
+
+let isPaused = false;
+if (!isPaused) {
+  console.log(`Motion On`);
+} else {
+  console.log(`Motion Off`);
 }
