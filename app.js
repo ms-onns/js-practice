1e2af2871ca1d2e2895a09ef85d919428d6e0215
Max Sheyn <nmmn1905@gmail.com>
2026-03-13 16:39:58 +0200

    feat: implement for and for...of loops to iterate over arrays

--- app.js
+++ app.js
@@ -202,3 +202,30 @@ if (!clubGuests.includes(newVip)) {
   console.log("Цей гість вже в клубі");
 }
 console.log(clubGuests);
+console.log("-------------------------------------");
+
+// ==========================================
+// Цикли (Loops) - цикл for
+// ==========================================
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 5; i >= 1; i--) {
+  console.log(i);
+}
+
+// ===
+
+let clients = ["Mike", "Fred", "Li"];
+
+for (let i = 0; i < clients.length; i++) {
+  console.log(`Вітаємо ${clients[i]}`);
+}
+
+// ===
+
+let shoppingCart = ["Laptop", "Maus", "Keyboard"];
+for (let item of shoppingCart) {
+  console.log(`Товар у кошику ${item}`);
+}
